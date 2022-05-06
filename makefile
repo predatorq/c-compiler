c549dda6d6f03078617da27a40a0de62b3bc40c6
predatorq <54240007+predatorq@users.noreply.github.com>
2022-05-06 20:43:58 +0800

    update

--- makefile
+++ makefile
@@ -1,6 +1,12 @@
 VPATH = *.c samples
 VPATH = *.lex src
-out: tokens.txt
+out: action_chart.txt
+
+action_chart.txt: LL1.out tokens.txt
+	./$< tokens.txt grammar.txt >> $@
+
+LL1.out: LL1.cpp
+	g++ -o $@ $< -std=c++2a
 
 tokens.txt: clang.out 
 	./$< < samples/numbers.c > $@
@@ -12,4 +18,4 @@ lex.yy.c: clang.lex
 	flex $<
 
 clean: 
-	rm clang.out lex.yy.c tokens.txt
+	rm clang.out lex.yy.c tokens.txt LL1.out action_chart.txt
--- src/LL1.cpp
+++ src/LL1.cpp
@@ -1,70 +1,78 @@
-#include<iostream>
-#include<fstream>
-#include<map>
-#include<string>
-#include<vector>
-#include<utility>
-#include<stack>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
+#include <utility>
+#include <stack>
 using namespace std;
 
-vector<string> read_from_tokens(char* filename){
-    ifstream infile; 
+vector<string> read_from_tokens(char *filename)
+{
+    ifstream infile;
     string data, tmp;
     vector<string> output;
     output.clear();
     infile.open(filename);
-    cout << "Reading from the token file" << endl; 
+    cout << "Reading from the token file" << endl;
     infile >> data;
-    assert(data=="TOKEN-TYPE");
-    infile >> data; 
-    assert(data=="TOKEN-VALUE");
+    assert(data == "TOKEN-TYPE");
     infile >> data;
-    assert(data=="-------------------------------------------------");
-    while(infile>>data){
+    assert(data == "TOKEN-VALUE");
+    infile >> data;
+    assert(data == "-------------------------------------------------");
+    while (infile >> data)
+    {
         infile >> tmp;
         output.push_back(data);
     }
-    cout << "read tokens succeed!"<< endl;
+    cout << "read tokens succeed!" << endl;
     infile.close();
     return output;
 }
 
-map<pair<string,string>, vector<string> > read_from_grammar(char* filename){
-    ifstream infile; 
+map<pair<string, string>, vector<string> > read_from_grammar(char *filename)
+{
+    ifstream infile;
     string data1, data2, data3;
-    pair<string,string> tmp;
-    map<pair<string,string>, vector<string> > output;
+    pair<string, string> tmp;
+    map<pair<string, string>, vector<string> > output;
     output.clear();
     infile.open(filename);
-    cout << "Reading from the grammar file" << endl; 
+    cout << "Reading from the grammar file" << endl;
     infile >> data1;
-    assert(data1=="GRAMMAR_FOR_EXPERIMENT2");
+    assert(data1 == "GRAMMAR_FOR_EXPERIMENT2");
     infile >> data1;
-    assert(data1=="-------------------------------------------------");
-    while(infile>>data1){
-        if(data1=="@")break;
+    assert(data1 == "-------------------------------------------------");
+    while (infile >> data1)
+    {
+        if (data1 == "@")
+            break;
         infile >> data2;
-        pair<string,string> tmp(data1,data2);
+        pair<string, string> tmp(data1, data2);
         vector<string> tmp2;
         tmp2.empty();
-        while(infile>> data3){
-            if(data3=="@")break;
+        while (infile >> data3)
+        {
+            if (data3 == "@")
+                break;
             tmp2.push_back(data3);
-            cout << data3 << endl;
+            // cout << data3 << endl;
         }
         output[tmp] = tmp2;
     }
-    cout << "read grammars succeed!"<< endl;
+    cout << "read grammars succeed!" << endl;
     infile.close();
     return output;
 }
 
-int main(int argc,char **argv){
+int main(int argc, char **argv)
+{
     // true表示表达式正确, false为错误
     bool flag = true;
     vector<string> tokens = read_from_tokens(argv[1]);
     // pair<string,string> 对应预测分析表的行、列, vector<string> 为空格中的字符
-    map<pair<string,string>, vector<string> > table = read_from_grammar(argv[2]);
+    map<pair<string, string>, vector<string> > table = read_from_grammar(argv[2]);
     stack<string> parse_stack;
     stack<string> remaining_input;
     parse_stack.empty();
@@ -76,18 +84,45 @@ int main(int argc,char **argv){
     remaining_input.push("$");
     for (int i = 0; i < tokens.size(); i++)
     {
-        remaining_input.push(tokens[tokens.size()-i-1]);
+        remaining_input.push(tokens[tokens.size() - i - 1]);
     }
 
-    
-    while(remaining_input.size()>0){
+    while (remaining_input.size() > 0)
+    {
         string remain = remaining_input.top();
         remaining_input.pop();
         string parse = parse_stack.top();
         parse_stack.pop();
-    }
-    
+        // cout << parse << "   "<< remain << endl;
+        pair<string, string> tmp(parse,remain);
+        if (remain == parse)
+        {
+            cout << "Match" << "  "<<remain << endl;
+            continue;
+        }
+        remaining_input.push(remain);
+        if (table.find(tmp) == table.end())
+        {
+            flag = false;
+            cout << "Meet error!" << endl;
+            break;
+        }
+        else
+        {
+            vector<string> tmp2 = table[tmp];
+            if(tmp2.size()==1&&tmp2[0]=="e")continue;
+            for (int i = 0; i < tmp2.size(); i++)
+            {
+                parse_stack.push(tmp2[tmp2.size() - i - 1]);
+            }
+            cout << parse << "->";
+            for(string i: tmp2){
+                cout << i;
+            }
+            cout << endl;
 
+        }
+    }
 
     return 0;
 }
